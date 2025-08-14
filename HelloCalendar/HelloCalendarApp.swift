4e3488b119dcc16302743c70d96c16dfb96d7c22
Mike Hummel <mike@mhus.de>
2025-08-14 17:30:46 +0200

    Initial Commit

---
+++ HelloCalendar/HelloCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCalendarApp.swift
+//  HelloCalendar
+//
+//  Created by Mike Hummel on 14.08.25.
+//
+
+import SwiftUI
+
+@main
+struct HelloCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloCalendarTests/HelloCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCalendarTests.swift
+//  HelloCalendarTests
+//
+//  Created by Mike Hummel on 14.08.25.
+//
+
+import Testing
+@testable import HelloCalendar
+
+struct HelloCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
